9a7f6b7226473e67f433217d25b80a6bf8d4a094
ficristo <ficristo.work@gmail.com>
2019-01-19 18:59:39 +0100

    Updated ESLint related dependencies
    Still need to fix all the errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
-  parser: "typescript-eslint-parser",
+  parser: "eslint-plugin-typescript/parser",
   parserOptions: { sourceType: "module" },
   extends: "pureprofile",
   env: { es6: true, node: true },
+  plugins: ["typescript"],
   rules: {
-    "indent": ["error", 4, { "SwitchCase": 1 }],
+    "indent": "off",
+    "typescript/indent": ["error", 4, { "SwitchCase": 1 }],
     "quotes": ["error", "double"],
     "no-const-assign": 0, // conflicts with ts
     "no-extra-parens": 0, // conflicts with ts
@@ -16,7 +18,8 @@ module.exports = {
     "no-use-before-define": 0, // conflicts with ts
     "require-await": 0, // this doesn"t work well with our express async middleware
     "sort-imports": 0, // this is too much effort to fix
-    "space-infix-ops": 0 // conflicts with ts
+    "space-infix-ops": 0, // conflicts with ts
+    "linebreak-style": 0 // too many warnings
   },
   globals: {
     ___dirname: false
